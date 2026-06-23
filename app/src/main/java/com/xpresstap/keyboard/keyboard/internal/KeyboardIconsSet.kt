0302d547c199f050119cdaac489aecfc71ba6ad5
tsuiwilliam <tsuiwilliam@gmail.com>
2026-06-22 23:09:19 -0700

    fix: add NFC_PAY branch to KeyboardIconsSet when expressions

--- app/src/main/java/com/xpresstap/keyboard/keyboard/internal/KeyboardIconsSet.kt
+++ app/src/main/java/com/xpresstap/keyboard/keyboard/internal/KeyboardIconsSet.kt
@@ -156,6 +156,7 @@ class KeyboardIconsSet private constructor() {
                     ToolbarKey.PAGE_END -> R.drawable.ic_page_end
                     ToolbarKey.SPLIT -> R.drawable.ic_ime_switcher
                     ToolbarKey.BACKGROUND_GATHERING -> R.drawable.ic_settings_gesture
+                    ToolbarKey.NFC_PAY -> R.drawable.ic_settings_gesture
                 })
             }
         } }
@@ -219,6 +220,7 @@ class KeyboardIconsSet private constructor() {
                     ToolbarKey.PAGE_END -> R.drawable.ic_page_end
                     ToolbarKey.SPLIT -> R.drawable.ic_ime_switcher
                     ToolbarKey.BACKGROUND_GATHERING -> R.drawable.ic_settings_gesture
+                    ToolbarKey.NFC_PAY -> R.drawable.ic_settings_gesture
                 })
             }
         } }
@@ -282,6 +284,7 @@ class KeyboardIconsSet private constructor() {
                     ToolbarKey.PAGE_END -> R.drawable.ic_page_end_rounded
                     ToolbarKey.SPLIT -> R.drawable.ic_ime_switcher
                     ToolbarKey.BACKGROUND_GATHERING -> R.drawable.ic_settings_gesture
+                    ToolbarKey.NFC_PAY -> R.drawable.ic_settings_gesture
                 })
             }
         } }
